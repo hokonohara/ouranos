718e5bbd1df948c3bf0bf57bcc68dddbd6c1ed1d
hokonohara <koichi.hokonohara@gmail.com>
2024-07-24 15:24:49 +0100

    latest

--- startup.sh
+++ startup.sh
@@ -2,20 +2,20 @@ cd -/workspace/user-authentication-system
 docker compose up -d
 sleep 5
 
-read -n 1
+read -n 1 -p "press any key to continue"
 
 export POSTGRESQL_URL='postgres://dhuser:passw0rd@localhost:5432/dhlocal?sslmode=disable'
 migrate -path setup/migrations -database ${POSTGRESQL_URL} up
 
-read -n 1
+read -n 1 -p "press any key to continue"
 
 ./setup/setup_seeds.sh
 
-read -n 1
+read -n 1 -p "press any key to continue"
 
 make idp-add-local
 
-read -n 1
+read -n 1 -p "press any key to continue"
 
 docker run \
  -v ~/workspace/data-transaction-system/config/:/app/config/ \
@@ -24,7 +24,7 @@ docker run \
  -p 8080:8080 \
  --name data-spaces-backend data-spaces-backend
 
-read -n 1
+read -n 1 -p "press any key to continue"
 
 docker run \
  -v ~/workspace/user-authentication-system/config/:/app/config/ \
@@ -33,6 +33,6 @@ docker run \
  -p 8081:8081 \
  --name authenticator-backend authenticator-backend
 
- read -n 1
+ read -n 1 -p "press any key to continue"
 
  docker ps -a --format "{{.Names}}\t{{.State}}"
